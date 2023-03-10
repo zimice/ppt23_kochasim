62eb00c88985c89ebe23660fe211e33974f475cd
zimice <simon.kochanek@me.com>
2023-03-10 11:31:43 +0100

    started working on cv3

---
+++ firstProgBlazor/ViewModels/VybaveniVm.cs
@@ -0,0 +1,48 @@
+﻿namespace firstProgBlazor.ViewModels
+{
+
+    public class VybaveniVm
+    {
+        public string Name { get; set; } = "";
+        
+        public bool isRevisionNeeded { get; set; }
+        public DateTime BoughtDateTime { get; set; }
+
+        public DateTime LastRevisionDateTime { get; set; }
+
+        public VybaveniVm(string Name, bool isRevisionNeeded, DateTime BoughtDateTime,DateTime LastRevisionDateTime) {
+            this.Name = Name;
+            this.isRevisionNeeded = isRevisionNeeded;  
+            this.BoughtDateTime = BoughtDateTime;
+            this.LastRevisionDateTime = LastRevisionDateTime;
+        }
+
+        public static List<VybaveniVm> VratRandSeznam(int pocet)
+        {
+            List<VybaveniVm> list = new List<VybaveniVm>();
+            int size = Random.Shared.Next(4, 20);
+            var rndboughtDateTime = new DateTime(2010, 1, 1).AddDays(Random.Shared.Next(1, (DateTime.Now - new DateTime(2010, 1, 1)).Days);
+            var rndlastRevisionDays = Random.Shared.Next(1, (DateTime.Now - rndboughtDateTime).Days);
+            var rndlastRevisionDateTime = rndboughtDateTime.AddDays(rndlastRevisionDays);
+            for (int i = 0; i < size;)
+                list.Add(new VybaveniVm(Random.Shared.Next(54000, 99999).ToString(), Convert.ToBoolean(Random.Shared.Next(2)), rndboughtDateTime, rndlastRevisionDateTime);
+)
+            return list;
+        }
+        public static DateTime GetRandomDateTime(DateTime? min = null, DateTime? max = null)
+        {
+            min = min ?? new DateTime(2020, 01, 01);
+            max = max ?? new DateTime(9999, 12, 31);
+
+            var range = max.Value - min.Value;
+            var randomUpperBound = (Int32)range.TotalSeconds;
+            if (randomUpperBound <= 0)
+                randomUpperBound = Random.Shared.Next(1, Int32.MaxValue);
+
+            var randTimeSpan = TimeSpan.FromSeconds((Int64)(range.TotalSeconds - Random.Shared.Next(0, randomUpperBound)));
+            return min.Value.Add(randTimeSpan);
+        }
+
+    }
+}
+    

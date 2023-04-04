6b9f758ac1abaa1e3f209573020532bfc5f3ffea
zimice <simon.kochanek@me.com>
2023-04-04 16:38:35 +0200

    still working on it

---
+++ Ppt.Shared/RevizeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ppt.Shared
+{
+	public class RevizeViewModel
+	{
+		public string nazev { get;set; }
+		public Guid Id { get; set; }
+		public RevizeViewModel(string nazev) { 
+			this.nazev = nazev;
+			this.Id = Guid.NewGuid();
+		}
+		public static RevizeViewModel generateRand()
+		{
+			return new RevizeViewModel(Random.Shared.Next(54000, 99999).ToString());
+		}
+	}
+	
+}

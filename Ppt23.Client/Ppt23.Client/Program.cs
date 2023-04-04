ccc8bf1f9d883465f8b8e2220713de9c2807c244
zimice <simon.kochanek@me.com>
2023-04-04 16:00:46 +0200

    last work before test

--- Ppt23.Client/Ppt23.Client/Program.cs
+++ Ppt23.Client/Ppt23.Client/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Ppt23.Client;
 
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:2222") });
+
 
 await builder.Build().RunAsync();
